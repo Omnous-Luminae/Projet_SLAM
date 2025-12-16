4a1112a84cbd8d74ed1167f66114d88bf9644d89
Omnous-Luminae <enjolras.ethan3@gmail.com>
2025-12-16 10:05:24 +0000

    commit

--- Projet_HAP-House_After_Party--dev/Projet_HAP(House_After_Party)/sql/add_total_cost_to_reservation.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Add total_cost field to Reservation table
--- Run this on your MySQL server (test on a copy first)
-
-ALTER TABLE Reservation ADD COLUMN total_cost DECIMAL(10,2) NULL AFTER id_Tarif;
